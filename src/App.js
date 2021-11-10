48e43f757f9901e5c2078f9145941f808fe6db17
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-10 18:16:40 +0800

    Added functionality to allow logged-in users to add new blogs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import LoginForm from './components/LoginForm'
 import Notification from './components/Notification'
+import CreateBlogForm from './components/CreateBlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -12,7 +13,9 @@ const App = () => {
 	const [errorMessage, setErrorMessage] = useState(null)
 	const [notificationMessage, setNotificationMessage] = useState(null)
 	const [user, setUser] = useState(null)
-
+	const [title, setTitle] = useState('')
+	const [url, setUrl] = useState('')
+	const [author, setAuthor] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -25,6 +28,7 @@ const App = () => {
 		if (loggedBlogAppUserJSON) {
 			const user = JSON.parse(loggedBlogAppUserJSON)
 			setUser(user)
+			blogService.setToken(user.token)
 		}
 	}, [])
 
@@ -40,6 +44,7 @@ const App = () => {
 				'loggedBlogAppUser', JSON.stringify(user)
 			)
 
+			blogService.setToken(user.token)
 			setUser(user)
 			setNotificationMessage('Successfully logged in')
 			setUsername('')
@@ -63,12 +68,43 @@ const App = () => {
 			window.localStorage.removeItem('loggedBlogAppUser')
 			setUser(null)
 			setNotificationMessage('Successfully logged out')
+			setTitle('')
+			setAuthor('')
+			setUrl('')
 			
 			setTimeout(() => {
 				setNotificationMessage(null)
 			}, 5000)
 		} catch(exception) {
 
+		}
+	}
+
+	const handleCreateNewBlog = async (event) => {
+		event.preventDefault()
+
+		try {
+			const blogObject = {
+				title: title,
+				author: author,
+				url: url
+			}
+
+			const returnedBlog = await blogService.create(blogObject)
+			setBlogs(blogs.concat(returnedBlog))
+			setNotificationMessage(`A new blog ${returnedBlog.title} by ${returnedBlog.author} added!`)
+			setTitle('')
+			setAuthor('')
+			setUrl('')
+			
+			setTimeout(() => {
+				setNotificationMessage(null)
+			}, 5000)
+		} catch(exception) {
+			setErrorMessage('Blog not added')
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, 5000)
 		}
 	}
 
@@ -79,17 +115,29 @@ const App = () => {
 				<Notification message={errorMessage} flag={'error'} />
 			}
 			{ user === null ?
-				<LoginForm 
-					handleLogin={handleLogin}
-					username={username}
-					setUsername={setUsername}
-					password={password}
-					setPassword={setPassword}
-				/> :
+				<div>
+					<LoginForm 
+						handleLogin={handleLogin}
+						username={username}
+						setUsername={setUsername}
+						password={password}
+						setPassword={setPassword}
+					/>
+				</div>
+				:
 				<div>
       		<h2>blogs</h2>
 					<p>{`${user.name} logged in`} <button type="submit" onClick={handleLogout}>logout</button></p>
-      		{blogs.map(blog => {
+      		<CreateBlogForm
+						handleCreateNewBlog={handleCreateNewBlog}
+						title={title}
+						author={author}
+						url={url}
+						setTitle={setTitle}
+						setAuthor={setAuthor}
+						setUrl={setUrl}
+					/>
+					{blogs.map(blog => {
 						return <Blog key={blog.id} blog={blog} />
 					})
 					}
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,27 +2,27 @@ import React from "react";
 
 const LoginForm = ({handleLogin, username, setUsername, password, setPassword}) => (
 	<form onSubmit={handleLogin}>
-        <h2>Log in to application</h2>
-				<div>
-          username
-            <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-            <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
+		<h2>Log in to application</h2>
+		<div>
+			username
+				<input
+				type="text"
+				value={username}
+				name="Username"
+				onChange={({ target }) => setUsername(target.value)}
+			/>
+		</div>
+		<div>
+			password
+				<input
+				type="password"
+				value={password}
+				name="Password"
+				onChange={({ target }) => setPassword(target.value)}
+			/>
+		</div>
+		<button type="submit">login</button>
+  </form>
 )
 
 export default LoginForm

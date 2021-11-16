6d868969eb381ad6bdfddc7b8a173587c418c7d1
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-16 14:36:18 +0800

    Wrote test to check that the blog's url and number of likes are shown when the button controlling the shown details has been clicked

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,19 +1,22 @@
 /* eslint-disable linebreak-style */
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: 'This is a test title',
-    url: 'thisisatest.com',
-    likes: 0,
-    author: 'This is a test author'
+const blog = {
+  title: 'This is a test title',
+  url: 'thisisatest.com',
+  likes: 0,
+  author: 'This is a test author',
+  user: {
+    name: 'Koh Jun Jie',
   }
+}
+
+test('renders content', () => {
 
   const mockHandler = jest.fn()
-
   const component = render(
     <Blog
       blog={blog}
@@ -22,19 +25,27 @@ test('renders content', () => {
       removeBlog={mockHandler} />
   )
 
-  expect(component.container).toHaveTextContent(
-    'This is a test title'
+  expect(component.container).toHaveTextContent('This is a test title')
+  expect(component.container).toHaveTextContent('This is a test author')
+  expect(component.container).not.toHaveTextContent('thisisatest.com')
+  expect(component.container).not.toHaveTextContent(0)
+})
+
+test('clicking the view button renders url and likes', () => {
+  const mockHandler = jest.fn()
+  const component = render(
+    <Blog
+      blog={blog}
+      incrementLikes={mockHandler}
+      nameOfCreator='test creator'
+      removeBlog={mockHandler} />
   )
 
-  expect(component.container).toHaveTextContent(
-    'This is a test author'
-  )
+  const button = component.getByText('view')
+  fireEvent.click(button)
 
-  expect(component.container).not.toHaveTextContent(
-    'thisisatest.com'
-  )
-
-  expect(component.container).not.toHaveValue(
-    0
-  )
+  expect(component.container).toHaveTextContent('This is a test title')
+  expect(component.container).toHaveTextContent('This is a test author')
+  expect(component.container).toHaveTextContent('thisisatest.com')
+  expect(component.container).toHaveTextContent(0)
 })

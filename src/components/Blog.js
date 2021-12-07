c60952676774ca1eafffa620c85ae3b1d35441b1
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-12-07 20:06:50 +0800

    Added display of comments from backend

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,10 +3,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Blog = ({ blog, handleLike, handleRemove, loggedInUser }) => {
-  // const [visible, setVisible] = useState(false)
 
+  if (!blog)
+    return null
 
-  // const label = visible ? 'hide' : 'view'
   const own = blog.user.username === loggedInUser.username
 
   return (
@@ -16,16 +16,17 @@ const Blog = ({ blog, handleLike, handleRemove, loggedInUser }) => {
       <div>likes {blog.likes} <button onClick={() => handleLike(blog)}>like</button></div>
       <div>added by {blog.user.name}</div>
       {own && <button onClick={() => handleRemove(blog.id)}>remove</button>}
+      <h2>comments</h2>
+      <ul>
+        {blog.comments.map(comment =>
+          <li key={comment}>{comment}</li>
+        )}
+      </ul>
     </div>
   )
 }
 
 Blog.propTypes = {
-  blog: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-  }).isRequired,
   handleLike: PropTypes.func.isRequired,
   handleRemove: PropTypes.func.isRequired,
 }
